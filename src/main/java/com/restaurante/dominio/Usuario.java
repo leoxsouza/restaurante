522976315bc7e1a87e395918197ab42399ac250f
Leonardo Oliveira <leonardo.oliveira@basis.com.br>
2020-09-10 10:32:23 -0300

    Changelog da tabela de usuario

--- src/main/java/com/restaurante/dominio/Usuario.java
+++ src/main/java/com/restaurante/dominio/Usuario.java
@@ -4,27 +4,34 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 
 @Entity
 @Getter
 @Setter
 @EqualsAndHashCode
+@Table(name="TB_USUARIO")
 public class Usuario implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID_USUARIO")
     private Long id;
 
+    @Column(name = "DS_LOGIN")
     private String login;
 
+    @Column(name = "DS_SENHA")
     private String senha;
 
+    @Column(name = "DS_NOME")
     private String nome;
 }
